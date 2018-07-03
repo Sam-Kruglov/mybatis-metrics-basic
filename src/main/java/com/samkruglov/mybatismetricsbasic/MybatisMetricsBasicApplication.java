67622d577fb4c6ef76512afb8e69a926f6656f92
Sam Kruglov <s.kruglov@tinkoff.ru>
2018-07-03 15:17:44 +0300

    initial commit

---
+++ src/main/java/com/samkruglov/mybatismetricsbasic/MybatisMetricsBasicApplication.java
@@ -0,0 +1,13 @@
+package com.samkruglov.mybatismetricsbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisMetricsBasicApplication {
+    
+    public static void main(String[] args) {
+        
+        SpringApplication.run(MybatisMetricsBasicApplication.class, args);
+    }
+}
