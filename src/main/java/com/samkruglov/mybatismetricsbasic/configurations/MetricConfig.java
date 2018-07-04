88a75f029e0e265857f8141d9af0d79fb8a14002
Sam Kruglov <s.kruglov@tinkoff.ru>
2018-07-04 15:14:05 +0300

    add Slf4jReporter

--- src/main/java/com/samkruglov/mybatismetricsbasic/configurations/MetricConfig.java
+++ src/main/java/com/samkruglov/mybatismetricsbasic/configurations/MetricConfig.java
@@ -1,10 +1,14 @@
 package com.samkruglov.mybatismetricsbasic.configurations;
 
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Slf4jReporter;
 import com.codahale.metrics.jmx.JmxReporter;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class MetricConfig {
@@ -12,6 +16,8 @@ public class MetricConfig {
     public static final String MYBATIS_METRIC_REGISTRY_BEAN = "mybatisMetricRegistry";
     
     public static final String MYBATIS_JMX_REPORTER_BEAN = "mybatisJmxReporter";
+    
+    public static final String MYBATIS_SLF4J_REPORTER_BEAN = "mybatisSlf4jReporter";
     
     @Bean(MYBATIS_METRIC_REGISTRY_BEAN)
     public MetricRegistry mybatisMetricRegistry() {
@@ -21,13 +27,35 @@ public class MetricConfig {
     
     /**
      * Once the reporter is started, all of the metrics in the registry will become visible via JConsole or VisualVM
-     * (if you install the MBeans plugin)
+     * (if you install the MBeans plugin). We keep it in the context so it lives.
+     *
+     * Note from DropWizard:
+     * We don’t recommend that you try to gather metrics from your production environment. JMX’s RPC API is fragile
+     * and bonkers. For development purposes and browsing, though, it can be very useful.
      */
     @Bean(MYBATIS_JMX_REPORTER_BEAN)
     public JmxReporter mybatisJmxReporter(@Qualifier(MYBATIS_METRIC_REGISTRY_BEAN) MetricRegistry registry) {
-        
+    
         JmxReporter reporter = JmxReporter.forRegistry(registry).build();
         reporter.start();
+    
+        return reporter;
+    }
+    
+    /*
+     * Once the reporter is started, all of the metrics in the registry will be logged once a minute (at the same time).
+     */
+    @Bean(MYBATIS_SLF4J_REPORTER_BEAN)
+    public Slf4jReporter mybatisSlf4jReporter(@Qualifier(MYBATIS_METRIC_REGISTRY_BEAN) MetricRegistry registry) {
+        
+        final Slf4jReporter reporter =
+                Slf4jReporter.forRegistry(registry)
+                             .outputTo(LoggerFactory.getLogger("metrics.mybatis"))
+                             .withLoggingLevel(Slf4jReporter.LoggingLevel.DEBUG)
+                             .convertRatesTo(TimeUnit.SECONDS)
+                             .convertDurationsTo(TimeUnit.MILLISECONDS)
+                             .build();
+        reporter.start(1, TimeUnit.MINUTES);
         
         return reporter;
     }
